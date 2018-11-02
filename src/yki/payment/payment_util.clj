d4cfb14d0a0a0adf41a325ad3f37627d18c9b02b
Tom Bertell <tom.bertell@siili.com>
2018-11-02 11:36:24 +0200

    URL_NOTIFY added to params

--- src/yki/payment/payment_util.clj
+++ src/yki/payment/payment_util.clj
@@ -7,24 +7,25 @@
            [java.util Locale]
            [org.apache.commons.codec.digest DigestUtils]))
 
-(defn- calculate-authcode [{::ys/keys [MERCHANT_ID LOCALE URL_SUCCESS URL_CANCEL
+(defn- calculate-authcode [{::ys/keys [MERCHANT_ID LOCALE URL_SUCCESS URL_CANCEL URL_NOTIFY
                                        AMOUNT ORDER_NUMBER REFERENCE_NUMBER MSG_SETTLEMENT_PAYER
                                        MSG_UI_MERCHANT_PANEL PARAMS_IN PARAMS_OUT]} secret]
-  (let [plaintext (str/join "|" (->> [secret MERCHANT_ID LOCALE URL_SUCCESS URL_CANCEL
+  (let [plaintext (str/join "|" (->> [secret MERCHANT_ID LOCALE URL_SUCCESS URL_CANCEL URL_NOTIFY
                                       AMOUNT ORDER_NUMBER REFERENCE_NUMBER  MSG_SETTLEMENT_PAYER
                                       MSG_UI_MERCHANT_PANEL PARAMS_IN PARAMS_OUT]
                                      (remove nil?)))]
     (-> plaintext (.getBytes "ISO-8859-1") DigestUtils/sha256Hex str/upper-case)))
 
-(defn generate-form-data [{:keys [paytrail-host yki-paytrail-uri merchant-id merchant-secret amount msg]}
+(defn generate-form-data [{:keys [paytrail-host yki-payment-uri merchant-id merchant-secret amount msg]}
                           {:keys [language-code order-number reference-number] :as params}]
-  (let [params-in     "MERCHANT_ID,LOCALE,URL_SUCCESS,URL_CANCEL,AMOUNT,ORDER_NUMBER,REFERENCE_NUMBER,MSG_SETTLEMENT_PAYER,MSG_UI_MERCHANT_PANEL,PARAMS_IN,PARAMS_OUT"
+  (let [params-in     "MERCHANT_ID,LOCALE,URL_SUCCESS,URL_CANCEL,URL_NOTIFY,AMOUNT,ORDER_NUMBER,REFERENCE_NUMBER,MSG_SETTLEMENT_PAYER,MSG_UI_MERCHANT_PANEL,PARAMS_IN,PARAMS_OUT"
         params-out    "ORDER_NUMBER,PAYMENT_ID,AMOUNT,TIMESTAMP,STATUS"
         localized-msg ((keyword language-code) msg)
         form-params {:MERCHANT_ID  merchant-id
                      :LOCALE       (case language-code "fi" "fi_FI" "sv" "sv_SE")
-                     :URL_SUCCESS  (str yki-paytrail-uri "/success")
-                     :URL_CANCEL   (str yki-paytrail-uri "/cancel")
+                     :URL_SUCCESS  (str yki-payment-uri "/success")
+                     :URL_CANCEL   (str yki-payment-uri "/cancel")
+                     :URL_NOTIFY   (str yki-payment-uri "/notify")
                      :AMOUNT       amount
                      :ORDER_NUMBER order-number
                      :REFERENCE_NUMBER reference-number
--- test/yki/payment/payment_util_test.clj
+++ test/yki/payment/payment_util_test.clj
@@ -11,8 +11,9 @@
                                                  :AMOUNT "100.00"
                                                  :PARAMS_OUT "ORDER_NUMBER,PAYMENT_ID,AMOUNT,TIMESTAMP,STATUS"
                                                  :URL_CANCEL "http://localhost:8080/payment/cancel"
+                                                 :URL_NOTIFY "http://localhost:8080/payment/notify"
                                                  :LOCALE "fi_FI"
-                                                 :PARAMS_IN "MERCHANT_ID,LOCALE,URL_SUCCESS,URL_CANCEL,AMOUNT,ORDER_NUMBER,REFERENCE_NUMBER,MSG_SETTLEMENT_PAYER,MSG_UI_MERCHANT_PANEL,PARAMS_IN,PARAMS_OUT"
+                                                 :PARAMS_IN "MERCHANT_ID,LOCALE,URL_SUCCESS,URL_CANCEL,URL_NOTIFY,AMOUNT,ORDER_NUMBER,REFERENCE_NUMBER,MSG_SETTLEMENT_PAYER,MSG_UI_MERCHANT_PANEL,PARAMS_IN,PARAMS_OUT"
                                                  :AUTHCODE "73EF2A4EDD7A7FBF07FD5F6FAF99674DC0C25A025FD74C221F4C35849E5C0FB3"
                                                  :MSG_SETTLEMENT_PAYER "msg_fi"
                                                  :MSG_UI_MERCHANT_PANEL "msg_fi"
@@ -20,7 +21,7 @@
 
 (deftest generate-payment-form-data
   (let [payment-config {:paytrail-host "https://paytrail.com"
-                        :yki-paytrail-uri "http://localhost:8080/payment"
+                        :yki-payment-uri "http://localhost:8080/payment"
                         :merchant-id 12345
                         :amount "100.00"
                         :merchant-secret "SECRET_KEY"
