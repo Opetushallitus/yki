a4cadf11446a224c24637d87833a60263c030194
Tom Bertell <tom.bertell@siili.com>
2018-11-12 13:50:57 +0200

    poll email queue once a second

--- src/yki/job/scheduled_tasks.clj
+++ src/yki/job/scheduled_tasks.clj
@@ -13,10 +13,11 @@
 
 (defmethod ig/init-key :yki.job.scheduled-tasks/payment-state-handler [_ {:keys [db]}]
   #(if (= (job-db/try-to-acquire-lock! db worker-id "PAYMENT_STATE_CHECKER" "9 MINUTES") 1)
-     (info "payment-state-handler " (l/local-now))))
+     (info "payment-state-handler" (l/local-now))))
 
 (defmethod ig/init-key :yki.job.scheduled-tasks/email-queue-reader [_ {:keys [email-q url-helper]}]
   #(pgq/take-with [email-request email-q]
                   (do
-                    (info "Received request:" email-request)
-                    (email/send-email url-helper email-request))))
+                    (if email-request
+                      (do (info "Email queue size" (pgq/count email-q))
+                          (email/send-email url-helper email-request))))))
