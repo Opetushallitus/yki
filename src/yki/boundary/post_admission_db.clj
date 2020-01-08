16188bfe70e5c132dd425d04a636afdf46ce159e
Esko Suomi <esko.suomi@siili.com>
2020-01-08 14:09:47 +0200

    remove extra printlns

--- src/yki/boundary/post_admission_db.clj
+++ src/yki/boundary/post_admission_db.clj
@@ -13,9 +13,6 @@
   duct.database.sql.Boundary
   (upsert-post-admission [{:keys [spec]} post-admission exam-session-id]
     (jdbc/with-db-transaction [tx spec]
-      (println "tx" tx)
-      (println "spec" spec)
-      (println "post-admission" post-admission)
       (q/upsert-post-admission! 
        spec 
        (into {:exam_session_id exam-session-id} (select-keys post-admission [:start_date :end_date :quota]))))))
