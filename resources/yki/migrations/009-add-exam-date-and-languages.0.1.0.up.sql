d5bfd46b4bcfce4f0a52240353f12a630377ba22
Juha <juha.repo@siili.com>
2020-09-10 15:47:21 +0300

    Updated migration to do nothing if data already exist in the database.

--- resources/yki/migrations/009-add-exam-date-and-languages.0.1.0.up.sql
+++ resources/yki/migrations/009-add-exam-date-and-languages.0.1.0.up.sql
@@ -1,5 +1,14 @@
-INSERT INTO exam_date(exam_date, registration_start_date, registration_end_date) VALUES ('2020-08-22', '2020-06-01', '2020-06-12');
+INSERT INTO exam_date(exam_date, registration_start_date, registration_end_date)
+     VALUES ('2020-08-22', '2020-06-01', '2020-06-12');
+ON CONFLICT (exam_date, registration_start_date, registration_end_date)
+DO NOTHING;
 --;;
-INSERT INTO exam_date_language(exam_date_id, language_code) VALUES ((SELECT id FROM exam_date WHERE exam_date = '2020-08-22'), 'eng');
+INSERT INTO exam_date_language(exam_date_id, language_code)
+     VALUES ((SELECT id FROM exam_date WHERE exam_date = '2020-08-22'), 'eng');
+ON CONFLICT (exam_date_id, language_code)
+DO NOTHING;
 --;;
-INSERT INTO exam_date_language(exam_date_id, language_code) VALUES ((SELECT id FROM exam_date WHERE exam_date = '2020-08-29'), 'fra');
+INSERT INTO exam_date_language(exam_date_id, language_code)
+     VALUES ((SELECT id FROM exam_date WHERE exam_date = '2020-08-22'), 'fra');
+ON CONFLICT (exam_date_id, language_code)
+DO NOTHING;
