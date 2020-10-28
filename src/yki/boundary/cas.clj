7e74f9311357693cb22cbe5a1a4878e42fab1f5f
Juha <juha.repo@siili.com>
2020-10-28 10:17:49 +0200

    Caller-Id testing

--- src/yki/boundary/cas.clj
+++ src/yki/boundary/cas.clj
@@ -18,16 +18,17 @@
 
 (defn- create-params [cas-session-id body]
   (cond-> {:headers {"Cookie" (str "JSESSIONID=" cas-session-id)}
+           :caller-id {"Caller-Id" "1.2.246.562.10.00000000001.yki"}
            :follow-redirects false}
     (some? body)
     (request-with-json-body body)))
 
 (defn- cas-http [cas-client cas-params method url & [body]]
-  (let [cas-session-id (.run (.fetchCasSession cas-client cas-params))
+  (let [cas-session-id (.run (.fetchCasSession cas-client cas-params "JSESSIONID"))
         resp (http-util/do-request (merge {:url url :method method}
                                           (create-params cas-session-id body)))]
     (if (= 302 (:status resp))
-      (let [new-cas-session-id (.run (.fetchCasSession cas-client cas-params))
+      (let [new-cas-session-id (.run (.fetchCasSession cas-client cas-params "JSESSIONID"))
             new-resp (http-util/do-request (merge {:url url :method method}
                                                   (create-params cas-session-id body)))]
         new-resp))
@@ -36,17 +37,17 @@
 (defrecord CasClient [url-helper cas-params]
   CasAccess
   (validate-ticket [_ ticket]
-    (let [cas-client (cas/cas-client (url-helper :cas-client))
+    (let [cas-client (cas/cas-client (url-helper :cas-client) "1.2.246.562.10.00000000001.yki")
           username   (.run (.validateServiceTicket cas-client (url-helper :yki.cas.login-success) ticket))]
       username))
 
   (cas-authenticated-get [_ url]
-    (cas-http (cas/cas-client (url-helper :cas-client)) cas-params :get url))
+    (cas-http (cas/cas-client (url-helper :cas-client) "1.2.246.562.10.00000000001.yki") cas-params :get url))
 
   (cas-authenticated-post [_ url body]
-    (cas-http (cas/cas-client (url-helper :cas-client)) cas-params :post url body))
+    (cas-http (cas/cas-client (url-helper :cas-client) "1.2.246.562.10.00000000001.yki") cas-params :post url body))
 
-  (get-cas-session [_ cas-params] (.run (.fetchCasSession (cas/cas-client (url-helper :cas-client) cas-params)))))
+  (get-cas-session [_ cas-params] (.run (.fetchCasSession (cas/cas-client (url-helper :cas-client) "1.2.246.562.10.00000000001.yki") cas-params "JSESSIONID"))))
 
 (defmethod ig/init-key :yki.boundary.cas/cas-client [_ {:keys [url-helper cas-creds]}]
   (fn [service]
