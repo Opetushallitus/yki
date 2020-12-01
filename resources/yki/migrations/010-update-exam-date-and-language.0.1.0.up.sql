1b44fe6ab3a3f46de8495b8c40fb18c83f4777c5
Johanna Paavola <johanna.paavola@siili.com>
2020-12-01 13:50:31 +0200

    updates postman docs and ragtime migrations

--- resources/yki/migrations/010-update-exam-date-and-language.0.1.0.up.sql
+++ resources/yki/migrations/010-update-exam-date-and-language.0.1.0.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE exam_date
-DROP CONSTRAINT exam_date_exam_date_registration_start_date_registration_en_key;
+DROP CONSTRAINT IF EXISTS exam_date_exam_date_registration_start_date_registration_en_key;
 
 ALTER TABLE exam_date
 ADD COLUMN IF NOT EXISTS post_admission_start_date DATE DEFAULT NULL,
@@ -7,7 +7,7 @@ ADD COLUMN IF NOT EXISTS post_admission_enabled BOOLEAN DEFAULT FALSE,
 ADD COLUMN IF NOT EXISTS deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL;
 
 ALTER TABLE exam_date_language
-DROP CONSTRAINT exam_date_language_exam_date_id_language_code_key;
+DROP CONSTRAINT IF EXISTS exam_date_language_exam_date_id_language_code_key;
 
 ALTER TABLE exam_date_language ADD COLUMN IF NOT EXISTS level_code TEXT REFERENCES exam_level (code) DEFAULT 'PERUS';
 ALTER TABLE exam_date_language ADD COLUMN IF NOT EXISTS deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL;
