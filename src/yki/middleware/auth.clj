acfe72a5e2223e2cc1f120affaa98c0cdd3051c6
Johanna <johanna.paavola@siili.com>
2021-05-26 11:02:46 +0300

    allows access to locale coded callbacks

--- src/yki/middleware/auth.clj
+++ src/yki/middleware/auth.clj
@@ -116,6 +116,9 @@
     :handler any-access}
    {:pattern #".*/auth/callback"
     :handler any-access}
+   {:pattern #"^.*/auth/callback.*"
+    :handler any-access
+    :request-method :get}
    {:pattern #".*/api/exam-session"
     :handler any-access
     :request-method :get}
