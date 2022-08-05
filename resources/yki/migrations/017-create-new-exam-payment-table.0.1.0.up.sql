e1a9d8705c903fe5c01d757b416733eb61beb91c
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-08-05 13:52:25 +0300

    Migration to create exam_payment_new

---
+++ resources/yki/migrations/017-create-new-exam-payment-table.0.1.0.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS exam_payment_new
+(
+    id              BIGSERIAL PRIMARY KEY,
+    state           payment_state                          NOT NULL,
+    registration_id BIGSERIAL REFERENCES registration (id) NOT NULL,
+    amount          NUMERIC                                NOT NULL,
+    reference       TEXT                                   NOT NULL UNIQUE,
+    transaction_id  TEXT                                   NOT NULL UNIQUE,
+    href            TEXT                                   NOT NULL UNIQUE,
+    paid_at         TIMESTAMP WITH TIME ZONE,
+    created         TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated         TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
+);
+
+CREATE INDEX IF NOT EXISTS exam_payment_new_state ON exam_payment_new (state);
+
+-- TODO Create a table as well for evaluation payments.
+-- TODO Index too.
