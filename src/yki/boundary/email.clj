84be5f6c9b265662916709d490ba4d764b55c3ee
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-12-08 15:40:16 +0200

    Handle attachments when sending emails

--- src/yki/boundary/email.clj
+++ src/yki/boundary/email.clj
@@ -9,23 +9,25 @@
   (log/info
     (str/join "\r\n"
               ["Email sending is disabled, logging instead:"
-                  (str "Recipients: " recipients)
-                  (str "Subject: " subject)
-                  (str "Body: " body)]))
+               (str "Recipients: " recipients)
+               (str "Subject: " subject)
+               (str "Body: " body)]))
   {:status 200})
 
 (defn send-email
-  [url-helper {:keys [recipients subject body]} disabled]
+  [url-helper {:keys [recipients subject body attachments]} disabled]
   (let [url                (url-helper :ryhmasahkoposti-service)
-        wrapped-recipients (mapv (fn [rcp] {:email rcp}) recipients)
-        response           (if disabled
-                             (log-disabled-email recipients subject body)
-                             (http-util/do-post url {:headers      {"content-type" "application/json; charset=UTF-8"}
-                                                     :query-params {:sanitize "false"}
-                                                     :body         (json/write-value-as-string {:email     {:subject subject
-                                                                                                            :html    true
-                                                                                                            :body    body
-                                                                                                            :charset "UTF-8"}
-                                                                                                :recipient wrapped-recipients})}))]
-    (when (not= 200 (:status response))
-      (throw (Exception. (str "Could not send email to " (str/join recipients)))))))
+        wrapped-recipients (mapv (fn [rcp] {:email rcp}) recipients)]
+    (if disabled
+      (log-disabled-email recipients subject body)
+      (let [email-data {:subject     subject
+                        :html        true
+                        :body        body
+                        :attachments attachments
+                        :charset     "UTF-8"}
+            response   (http-util/do-post url {:headers      {"content-type" "application/json; charset=UTF-8"}
+                                               :query-params {:sanitize "false"}
+                                               :body         (json/write-value-as-string {:email     email-data
+                                                                                          :recipient wrapped-recipients})})]
+        (when (not= 200 (:status response))
+          (throw (Exception. (str "Could not send email to " (str/join recipients)))))))))
