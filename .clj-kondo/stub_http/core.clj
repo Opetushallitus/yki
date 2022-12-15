1f23be8e34b71501b2324ce5c51f5299f37b835f
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-12-15 22:11:44 +0200

    Configure clj-kondo's macro expansion for stub-http.core/with-routes!

---
+++ .clj-kondo/stub_http/core.clj
@@ -0,0 +1,13 @@
+(ns stub-http.core)
+
+; Dummy implementation of with-routes! that let's clj-kondo know
+; that `server` and `port` are defined by the with-routes! macro.
+(defmacro with-routes! [bindings-or-routes & more-args]
+  (let [[bindings routes body] (if (vector? bindings-or-routes)
+                            [bindings-or-routes (first more-args) (rest more-args)]
+                            [[] bindings-or-routes more-args])]
+    `(let ~bindings
+       (let [~'server nil
+             ~'port (:port ~'server)]
+         ~routes
+         ~@body))))
