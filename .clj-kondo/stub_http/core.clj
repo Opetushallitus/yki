fdf51f578b5699f715ba5e851db2ab1df74f5a12
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-12-16 18:13:17 +0200

    Improve documentation of custom macro expansion configs

--- .clj-kondo/stub_http/core.clj
+++ .clj-kondo/stub_http/core.clj
@@ -1,6 +1,6 @@
 (ns stub-http.core)
 
-; Dummy implementation of with-routes! that let's clj-kondo know
+; Dummy implementation of with-routes! that lets clj-kondo know
 ; that `server` and `port` are defined by the with-routes! macro.
 (defmacro with-routes! [bindings-or-routes & more-args]
   (let [[bindings routes body] (if (vector? bindings-or-routes)
