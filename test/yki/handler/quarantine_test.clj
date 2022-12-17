7d47f746a52530266de26e3e1c9a4a518c118608
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-12-17 22:06:58 +0200

    Fix insertion test.
    For some reason, the previous method of mocking requests against handlers resulted in an empty body
    after destructuring in the handler. Switching from ring.mock.request to peridot changes this behaviour
    enough to fix the test.

--- test/yki/handler/quarantine_test.clj
+++ test/yki/handler/quarantine_test.clj
@@ -1,13 +1,25 @@
 (ns yki.handler.quarantine-test
-  (:require [clojure.test :refer [deftest use-fixtures testing is]]
-            [ring.mock.request :as mock]
-            [yki.embedded-db :as embedded-db]
-            [yki.boundary.quarantine-db :as quarantine-db]
-            [yki.handler.base-test :as base]
-            [yki.handler.routing :as routing]))
+  (:require
+    [clojure.test :refer [deftest use-fixtures testing is]]
+    [compojure.core :as core]
+    [integrant.core :as ig]
+    [ring.mock.request :as mock]
+    [stub-http.core :refer [with-routes!]]
+    [yki.embedded-db :as embedded-db]
+    [yki.handler.base-test :as base]
+    [yki.handler.routing :as routing]
+    [jsonista.core :as j]
+    [peridot.core :as peridot]))
 
 (use-fixtures :once embedded-db/with-postgres embedded-db/with-migration)
 (use-fixtures :each embedded-db/with-transaction)
+
+(defn create-handlers [port]
+  (let [access-log         (base/access-log)
+        db                 (base/db)
+        url-helper         (base/create-url-helper (str "localhost" port))
+        quarantine-handler (ig/init-key :yki.handler/quarantine {:db db :url-helper url-helper :access-log access-log})]
+    (core/routes quarantine-handler)))
 
 (deftest get-quarantine-test
   (base/insert-quarantine)
@@ -29,22 +41,34 @@
 
 (deftest delete-quarantine-test
   (base/insert-quarantine)
-  (let [request   (mock/request :delete (str routing/quarantine-api-root "/1"))
-        response  (base/send-request-with-tx request)]
+  (let [request  (mock/request :delete (str routing/quarantine-api-root "/1"))
+        response (base/send-request-with-tx request)]
     (testing "delete quarantine endpoint should return 200 and add new quarantine"
       (is (= (:status response) 200))
       (is (= 0 (:count (base/select-one "SELECT COUNT(*) FROM quarantine")))))))
 
 (deftest insert-quarantine-test
-  (let [request   (-> (mock/request :post routing/quarantine-api-root)
-                      (mock/json-body base/quarantine-form))
-        response  (base/send-request-with-tx request)]
-    (testing "insert quarantine endpoint should return 200 and add new quarantine"
-      (is true)
-      ; FIXME: mock request body is empty for some reason
-      ; (is (= {:id 1 :state "PAID_AND_CANCELLED" :reviewed_bool true :quarantined 1}
-      ;       (base/select "SELECT id, state, reviewed IS NOT NULL AS reviewed_bool, quarantined FROM registration WHERE id = 1")))
-      )))
+  (with-routes!
+    {}
+    (let [handler (create-handlers port)
+          session (peridot/session handler)]
+      (peridot/request
+        session
+        routing/quarantine-api-root
+        :request-method :post
+        :body (j/write-value-as-string base/quarantine-form)
+        :content-type "application/json")
+      (testing "insert quarantine endpoint should return 200 and add new quarantine"
+        (is (= {:id            1
+                :language_code "fin"
+                :end_date      "2022-12-30"
+                :birthdate     "1999-01-27"
+                :ssn           "301079-900U"
+                :name          "Max Syöttöpaine"
+                :email         "email@invalid.invalid"
+                :phone_number  "0401234567"}
+               (-> (base/select-one "SELECT * FROM quarantine")
+                   (dissoc :created :updated :deleted))))))))
 
 (deftest set-quarantine-test
   (base/insert-base-data)
