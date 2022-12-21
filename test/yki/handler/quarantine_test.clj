89e21246a9a9b854db7aaf5e3a3f986def375d11
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-12-21 11:34:39 +0200

    Add test coverage

--- test/yki/handler/quarantine_test.clj
+++ test/yki/handler/quarantine_test.clj
@@ -1,18 +1,18 @@
 (ns yki.handler.quarantine-test
   (:require
     [clojure.test :refer [deftest use-fixtures testing is]]
+    [clojure.data.json :as json]
+    [clojure.string :as str]
     [compojure.core :as core]
     [integrant.core :as ig]
+    [peridot.core :as peridot]
     [ring.mock.request :as mock]
     [stub-http.core :refer [with-routes!]]
     [yki.embedded-db :as embedded-db]
     [yki.handler.base-test :as base]
-    [yki.handler.routing :as routing]
-    [jsonista.core :as j]
-    [peridot.core :as peridot]))
+    [yki.handler.routing :as routing]))
 
-(use-fixtures :once embedded-db/with-postgres embedded-db/with-migration)
-(use-fixtures :each embedded-db/with-transaction)
+(use-fixtures :each embedded-db/with-postgres embedded-db/with-migration embedded-db/with-transaction)
 
 (defn create-handlers [port]
   (let [access-log         (base/access-log)
@@ -43,43 +43,79 @@
   (base/insert-quarantine)
   (let [request  (mock/request :delete (str routing/quarantine-api-root "/1"))
         response (base/send-request-with-tx request)]
-    (testing "delete quarantine endpoint should return 200 and add new quarantine"
+    (testing "delete quarantine endpoint should return 200 and delete existing quarantine"
       (is (= (:status response) 200))
       (is (= 0 (:count (base/select-one "SELECT COUNT(*) FROM quarantine")))))))
+
+(defn- request-with-json-body [session route method data]
+  (let [{response :response} (peridot/request
+                               session
+                               route
+                               :request-method method
+                               :body (json/write-str data)
+                               :content-type "application/json")
+        body (-> (:body response)
+                 (slurp)
+                 (json/read-str :key-fn keyword))]
+    {:status (:status response)
+     :body   body}))
 
 (deftest insert-quarantine-test
   (with-routes!
     {}
-    (let [handler (create-handlers port)
-          session (peridot/session handler)]
-      (peridot/request
-        session
-        routing/quarantine-api-root
-        :request-method :post
-        :body (j/write-value-as-string base/quarantine-form)
-        :content-type "application/json")
+    (let [handler  (create-handlers port)
+          session  (peridot/session handler)
+          response (request-with-json-body
+                     session
+                     routing/quarantine-api-root
+                     :post
+                     base/quarantine-form)]
       (testing "insert quarantine endpoint should return 200 and add new quarantine"
-        (is (= {:id            1
-                :language_code "fin"
-                :end_date      "2022-12-30"
-                :birthdate     "1999-01-27"
-                :ssn           "301079-900U"
-                :name          "Max Syöttöpaine"
-                :email         "email@invalid.invalid"
-                :phone_number  "0401234567"}
+        (is (= {:status 200
+                :body   {:success true}} response))
+        (is (= (assoc base/quarantine-form :id 1)
                (-> (base/select-one "SELECT * FROM quarantine")
                    (dissoc :created :updated :deleted))))))))
 
 (deftest set-quarantine-test
   (base/insert-base-data)
   (base/insert-registrations "SUBMITTED")
+  (base/insert-registrations "COMPLETED")
   (base/insert-quarantine)
-  (let [request   (-> (mock/request :put (str routing/quarantine-api-root "/1/registration/1/set"))
-                      (mock/json-body {:is_quarantined "true"}))
-        response  (base/send-request-with-tx request)]
-    (testing "set quarantine endpoint should return 200 and registration be cancelled"
-      (is true)
-      ; FIXME: mock request body is empty for some reason
-      ; (is (= {:id 1 :state "PAID_AND_CANCELLED" :reviewed_bool true :quarantined 1}
-      ;       (base/select "SELECT id, state, reviewed IS NOT NULL AS reviewed_bool, quarantined FROM registration WHERE id = 1")))
-      )))
+  (with-routes!
+    {}
+    (let [handler                           (create-handlers port)
+          session                           (peridot/session handler)
+          set-registration-quarantine-state (fn [registration-id quarantine-id quarantined?]
+                                              (request-with-json-body
+                                                session
+                                                (str/join "/" [routing/quarantine-api-root quarantine-id
+                                                               "registration"
+                                                               registration-id
+                                                               "set"])
+                                                :put
+                                                {:is_quarantined quarantined?}))
+          select-quarantine-details-for-registration #(base/select-one
+                                                        (str "SELECT id, state, reviewed IS NOT NULL AS reviewed_bool, quarantine_id FROM registration WHERE id = " % ";"))]
+      (testing "set quarantine endpoint should return 200 and cancel registration"
+        (is (= {:body   {:success true}
+                :status 200}
+               (set-registration-quarantine-state 1 1 true)))
+        (is (= {:id 1 :state "CANCELLED" :reviewed_bool true :quarantine_id 1}
+               (select-quarantine-details-for-registration 1)))
+        (is (= {:body {:success true}
+                :status 200}
+               (set-registration-quarantine-state 4 1 true)))
+        (is (= {:id 4 :state "PAID_AND_CANCELLED" :reviewed_bool true :quarantine_id 1}
+               (select-quarantine-details-for-registration 4))))
+      (testing "set quarantine endpoint should allow setting registration as not quarantined"
+        (is (= {:body   {:success true}
+                :status 200}
+               (set-registration-quarantine-state 1 1 false)))
+        (is (= {:id 1 :state "SUBMITTED" :reviewed_bool true :quarantine_id nil}
+               (select-quarantine-details-for-registration 1)))
+        (is (= {:body   {:success true}
+                :status 200}
+               (set-registration-quarantine-state 4 1 false)))
+        (is (= {:id 4 :state "PAID_AND_CANCELLED" :reviewed_bool true :quarantine_id nil}
+               (select-quarantine-details-for-registration 4)))))))
