5eee00f1be4f6f8cb932b55a1281170eff41a493
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-12-23 16:32:10 +0200

    Add middleware to inject a static OID into session identity for dev workflow

--- src/yki/middleware/no_auth.clj
+++ src/yki/middleware/no_auth.clj
@@ -5,10 +5,18 @@
             [ring.middleware.session.cookie :refer [cookie-store]]))
 
 (defmethod ig/init-key :yki.middleware.no-auth/with-authentication [_ {:keys [session-config]}]
-  (defn with-authentication [handler]
+  (fn with-authentication [handler]
     (warn "No authentication in use")
     (wrap-session
-     handler
-     {:store        (cookie-store {:key (.getBytes ^String (:key session-config))})
-      :cookie-name  "yki"
-      :cookie-attrs (:cookie-attrs session-config)})))
+      handler
+      {:store        (cookie-store {:key (.getBytes ^String (:key session-config))})
+       :cookie-name  "yki"
+       :cookie-attrs (:cookie-attrs session-config)})))
+
+(defmethod ig/init-key :yki.middleware.no-auth/with-fake-oid [_ {:keys [oid]}]
+  (fn with-authentication [handler]
+    (warn "No authentication in use, injecting a constant OID into the session identity.")
+    (fn [request]
+      (-> request
+          (assoc-in [:session :identity :oid] oid)
+          (handler)))))
