3bb70f5b16a8776c0b8ec282f5d719b0a81c65b5
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-01-09 12:07:18 +0200

    Read translations from provided resources instead of from the localisation service

--- src/yki/boundary/localisation.clj
+++ src/yki/boundary/localisation.clj
@@ -1,31 +1,25 @@
 (ns yki.boundary.localisation
-  (:require [clojure.core.memoize :as memo]
-            [jsonista.core :as json]
-            [yki.util.http-util :as http-util]))
+  (:require
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]
+    [clojure.tools.logging :as log])
+  (:import (java.io FileReader)))
 
-(defn- get-localisation
-  "Converts response returned from localisation service to simple map."
-  [category key url lang]
-  (let [response         (http-util/do-get url (into {} (remove #(nil? (val %)) {:category category
-                                                                                 :locale   lang
-                                                                                 :key      key})))
-        status           (:status response)
-        json             (json/read-value (:body response) (json/object-mapper {:decode-key-fn true}))
-        grouped-by-key   (group-by :key json)
-        flattened-to-map (reduce-kv #(assoc %1 %2 ((first %3) :value)) {} grouped-by-key)]
-    {:json   flattened-to-map
-     :status status}))
 
-(defonce one-hour (* 1000 60 60))
+(defn read-translations [lang]
+  (let [f (-> (str "yki/localisations/" lang ".json")
+              (io/resource)
+              (io/file))]
+    (with-open [rdr (FileReader. f)]
+      (json/read rdr))))
 
-(def get-localisation-memoized
-  (memo/ttl get-localisation :ttl/threshold one-hour))
+(defonce translations {"fi" (read-translations "fi")
+                       "en" (read-translations "en")
+                       "sv" (read-translations "sv")})
 
-(defn get-translations [url-helper category key lang]
-  (let [url      (url-helper :localisation.service)
-        response (get-localisation-memoized category key url lang)]
-    (when (= (:status response) 200)
-      (:json response))))
-
-(defn get-translation [url-helper key lang]
-  (get (get-translations url-helper "yki" key lang) key))
+(defn get-translation [_ key lang]
+  (if-let [translation (get-in translations [lang key])]
+    translation
+    (do
+      (log/error "Missing translation for language" lang "and translation" key)
+      key)))
