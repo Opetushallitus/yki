217b307be46f50b9805397e71b0fdbc233ab2389
Mika Huttunen <mika.huttunen@hotmail.fi>
2023-03-08 14:30:05 +0200

    OPHYKIKEH-161: removed new yki-ui-sub-domain config

--- src/yki/util/url_helper.clj
+++ src/yki/util/url_helper.clj
@@ -10,15 +10,14 @@
 
 (defmethod ig/init-key
   :yki.util/url-helper
-  [_ {:keys [virkailija-host oppija-host oppija-sub-domain yki-ui-sub-domain yki-register-host tunnistus-host yki-host-virkailija alb-host scheme]
-      :or   {virkailija-host "" tunnistus-host "" oppija-host "" oppija-sub-domain "" yki-ui-sub-domain "" yki-register-host "" yki-host-virkailija "" alb-host "" scheme "https"}}]
+  [_ {:keys [virkailija-host oppija-host oppija-sub-domain yki-register-host tunnistus-host yki-host-virkailija alb-host scheme]
+      :or   {virkailija-host "" tunnistus-host "" oppija-host "" oppija-sub-domain "" yki-register-host "" yki-host-virkailija "" alb-host "" scheme "https"}}]
   (reset! url-properties
           (doto (OphProperties. (into-array String ["/yki/yki-oph.properties"]))
             (.addDefault "scheme" scheme)
             (.addDefault "host-virkailija" virkailija-host)
             (.addDefault "host-oppija" oppija-host)
             (.addDefault "sub-domain-oppija" oppija-sub-domain)
-            (.addDefault "sub-domain-yki-ui" yki-ui-sub-domain)
             (.addDefault "host-tunnistus" tunnistus-host)
             (.addDefault "host-alb" alb-host)
             (.addDefault "host-yki-register" yki-register-host)
