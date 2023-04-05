dbdc43ed4e601ca9a480f6673df433b5200792c4
Mika Huttunen <mika.huttunen@hotmail.fi>
2023-04-05 14:32:16 +0300

    OPHYKIKEH-176: added check constraint

--- resources/yki/migrations/023-add-start-date-to-quarantine-table.0.1.0.up.sql
+++ resources/yki/migrations/023-add-start-date-to-quarantine-table.0.1.0.up.sql
@@ -3,3 +3,5 @@ ALTER TABLE quarantine ADD COLUMN IF NOT EXISTS start_date DATE;
 UPDATE quarantine SET start_date = created;
 
 ALTER TABLE quarantine ALTER COLUMN start_date SET NOT NULL;
+
+ALTER TABLE quarantine ADD CONSTRAINT ck_quarantine_start_date_end_date CHECK (start_date < end_date);
