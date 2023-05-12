1ec662b37fc4b8363d0ed8f20a7578307668193f
Mika Huttunen <mika.huttunen@hotmail.fi>
2023-05-12 10:28:58 +0300

    OPHYKIKEH-190: payment success mail updated

--- test/yki/util/template_util_test.clj
+++ test/yki/util/template_util_test.clj
@@ -38,3 +38,36 @@
       (is (s/includes? rendered "Examensavgift: 100,00 €"))
       (is (s/includes? rendered "Betala avgiften senast 24.4.2024, annars återkallas din anmälan."))
       (is (s/includes? rendered "http://localhost:8080/payment")))))
+
+(deftest render-payment-success-email-test
+  (let [base-data {:language       "Finnish"
+                   :level          "Basic level"
+                   :exam_date      "2024-05-16"
+                   :street_address "Katutie 13"
+                   :zip            "00500"
+                   :post_office    "Helsinki"
+                   :name           "Järjestäjä Oy"}]
+
+    (testing "mail contains proper content for base template data"
+      (let [rendered (template-util/render "PAYMENT_SUCCESS" "en" base-data)]
+        (is (s/includes? rendered "Test: Finnish basic level"))
+        (is (s/includes? rendered "Test day: 16.5.2024"))
+        (is (s/includes? rendered "Test centre: Järjestäjä Oy, Katutie 13, 00500 HELSINKI"))
+        (is (not (s/includes? rendered "Additional information from the organizer")))
+        (is (not (s/includes? rendered "Test centre's contact information")))))
+
+    (testing "mail contains proper content with extra information and organizer's contact info"
+      (let [template-data (assoc base-data :extra_information "Be on time"
+                                           :contact_info {:name         "Foo Bar"
+                                                          :email        "foo@bar"
+                                                          :phone_number "+358123"})
+            rendered      (template-util/render "PAYMENT_SUCCESS" "en" template-data)]
+        (is (s/includes? rendered "Test: Finnish basic level"))
+        (is (s/includes? rendered "Test day: 16.5.2024"))
+        (is (s/includes? rendered "Test centre: Järjestäjä Oy, Katutie 13, 00500 HELSINKI"))
+        (is (s/includes? rendered "Additional information from the organizer"))
+        (is (s/includes? rendered "Be on time"))
+        (is (s/includes? rendered "Test centre's contact information"))
+        (is (s/includes? rendered "Name: Foo Bar"))
+        (is (s/includes? rendered "Email address: foo@bar"))
+        (is (s/includes? rendered "Phone: +358123"))))))
