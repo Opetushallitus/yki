221ce5f5bb944c50f3c03a5052f2d48f9bddb642
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-11-17 14:32:44 +0200

    Use clj-json-patch to form patches for update operations instead of the patch generation library used by auditlogger.
    The implementation from auditlogger creates nonsensible patches especially for updates to nested / array data.

--- src/yki/util/audit_log.clj
+++ src/yki/util/audit_log.clj
@@ -1,16 +1,19 @@
 (ns yki.util.audit-log
-  (:require [clojure.tools.logging :as log]
-            [jsonista.core :as json]
-            [yki.util.log-util :as log-util])
+  (:require
+    [clj-json-patch.core :as patch]
+    [clojure.string :as str]
+    [clojure.tools.logging :as log]
+    [jsonista.core :as json]
+    [yki.util.log-util :as log-util])
   (:import [java.net InetAddress]
            [org.ietf.jgss Oid]
-           [com.google.gson JsonParser]
+           [com.google.gson JsonArray JsonParser]
            [com.fasterxml.jackson.datatype.joda JodaModule]
            [fi.vm.sade.auditlog Audit ApplicationType Logger Operation User Target$Builder Changes Changes$Builder]))
 
 (def mapper
   (json/object-mapper
-   {:modules [(JodaModule.)]}))
+    {:modules [(JodaModule.)]}))
 
 (def update-op "update")
 (def create-op "create")
@@ -29,9 +32,9 @@
 (defonce ^JsonParser jsonParser (JsonParser.))
 
 (defonce ^Logger logger-proxy
-  (reify Logger
-    (log [_ msg]
-      (log/info msg))))
+         (reify Logger
+           (log [_ msg]
+             (log/info msg))))
 
 (defn op ^Operation [operation]
   (reify Operation
@@ -45,15 +48,27 @@
 
 (defonce ^:private ^Audit oppija-logger (Audit. logger-proxy "yki" ApplicationType/OPPIJA))
 
-(defn- create-changes [change]
+(defn- format-json-path [^String path]
+  (-> (subs path 1)
+      (str/replace #"/" ".")))
+
+(defn- update->json-array [old new]
+  (let [json-patch (->> (patch/diff old new)
+                        (mapv #(update % "path" format-json-path)))]
+    (->> json-patch
+         ->json-string
+         (.parse jsonParser)
+         (.getAsJsonArray))))
+
+(defn- create-changes ^JsonArray [change]
   (case (:type change)
-    "update" (let [old  (.getAsJsonObject (.parse jsonParser (->json-string (:old change))))
-                   new  (.getAsJsonObject (.parse jsonParser (->json-string (:new change))))]
-               (Changes/updatedDto new old))
-    "delete" (.build (Changes$Builder.))
-    "cancel" (.build (Changes$Builder.))
-    "create" (let [new  (.getAsJsonObject (.parse jsonParser (->json-string (:new change))))]
-               (Changes/addedDto new))))
+    "update" (let [old (:old change)
+                   new (:new change)]
+               (update->json-array old new))
+    "delete" (.asJsonArray (.build (Changes$Builder.)))
+    "cancel" (.asJsonArray (.build (Changes$Builder.)))
+    "create" (let [new (.getAsJsonObject (.parse jsonParser (->json-string (:new change))))]
+               (.asJsonArray (Changes/addedDto new)))))
 
 (defn- oid-or-nil [^String oid]
   (when oid
@@ -62,16 +77,16 @@
 (defn log
   [{:keys [request target-kv change]}]
   (try
-    (let [inet-address    (InetAddress/getLocalHost)
-          session         (:session request)
-          oid             (get-in session [:identity :oid])
-          user-agent      ((:headers request) "user-agent")
-          user            (User. (oid-or-nil oid) inet-address (:yki-session-id session) user-agent)
-          op              (op (:type change))
-          target          (-> (Target$Builder.)
-                              (.setField (:k target-kv) (str (:v target-kv)))
-                              (.build))
-          changes         (create-changes change)]
+    (let [inet-address (InetAddress/getLocalHost)
+          session      (:session request)
+          oid          (get-in session [:identity :oid])
+          user-agent   ((:headers request) "user-agent")
+          user         (User. (oid-or-nil oid) inet-address (:yki-session-id session) user-agent)
+          op           (op (:type change))
+          target       (-> (Target$Builder.)
+                           (.setField (:k target-kv) (str (:v target-kv)))
+                           (.build))
+          changes      (create-changes change)]
       (.log virkailija-logger user op target changes))
     (catch Exception e
       (log-util/error e "Virkailija audit logging failed for data:" change))))
@@ -79,16 +94,16 @@
 (defn log-participant
   [{:keys [request target-kv change oid]}]
   (try
-    (let [inet-address    (InetAddress/getLocalHost)
-          session         (:session request)
-          oid             (or oid (get-in session [:identity :oid]))
-          user-agent      ((:headers request) "user-agent")
-          user            (User. (oid-or-nil oid) inet-address (:yki-session-id session) user-agent)
-          op              (op (:type change))
-          target          (-> (Target$Builder.)
-                              (.setField (:k target-kv) (str (:v target-kv)))
-                              (.build))
-          changes         (create-changes change)]
+    (let [inet-address (InetAddress/getLocalHost)
+          session      (:session request)
+          oid          (or oid (get-in session [:identity :oid]))
+          user-agent   ((:headers request) "user-agent")
+          user         (User. (oid-or-nil oid) inet-address (:yki-session-id session) user-agent)
+          op           (op (:type change))
+          target       (-> (Target$Builder.)
+                           (.setField (:k target-kv) (str (:v target-kv)))
+                           (.build))
+          changes      (create-changes change)]
       (.log oppija-logger user op target changes))
     (catch Exception e
       (log-util/error e "Participant audit logging failed for data:" change))))
