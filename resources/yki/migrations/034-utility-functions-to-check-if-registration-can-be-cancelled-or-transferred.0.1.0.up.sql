7bdb1ca56c847fe8b3992c0fa934303143f6db2b
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-05-09 15:16:24 +0300

    Introduce utility SQL functions to help checking if user can relocate or cancel registration

---
+++ resources/yki/migrations/034-utility-functions-to-check-if-registration-can-be-cancelled-or-transferred.0.1.0.up.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION select_registration_modification_dl(registration_id bigint) RETURNS TIMESTAMP WITH TIME ZONE
+    LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    RETURN date_trunc('day',
+        (SELECT ed.exam_date FROM registration r
+         INNER JOIN exam_session es ON r.exam_session_id = es.id
+         INNER JOIN exam_date ed ON es.exam_date_id = ed.id
+        WHERE r.id=registration_id)
+        AT TIME ZONE 'Europe/Helsinki');
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION is_transferable(registration_id bigint) RETURNS SETOF BOOLEAN
+    LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    RETURN QUERY SELECT EXISTS
+                            (SELECT r.id
+                             FROM registration r
+                             WHERE r.id = registration_id
+                               AND r.is_transfered = FALSE
+                               AND r.state = 'COMPLETED'
+                               AND current_timestamp < select_registration_modification_dl(r.id));
+END;
+$$;
