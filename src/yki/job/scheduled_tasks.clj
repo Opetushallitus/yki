3cbb038f3f623b57d650087151f99a804d9feefb
Laura Ketola <laura.ketola@gofore.com>
2025-05-27 20:42:10 +0300

    Add migrator for registration->person

--- src/yki/job/scheduled_tasks.clj
+++ src/yki/job/scheduled_tasks.clj
@@ -10,6 +10,7 @@
     [yki.boundary.exam-session-db :as exam-session-db]
     [yki.boundary.job-db :as job-db]
     [yki.boundary.onr :as onr]
+    [yki.boundary.person-db :as person]
     [yki.boundary.registration-db :as registration-db]
     [yki.boundary.yki-register :as yki-register]
     [yki.job.job-queue]
@@ -36,6 +37,10 @@
 (defonce sync-onr-participant-data-handler-conf {:worker-id (str (UUID/randomUUID))
                                                  :task "SYNC_ONR_PARTICIPANT_DATA_HANDLER"
                                                  :interval "59 MINUTES"})
+
+(defonce person-migrator-conf {:worker-id (str (UUID/randomUUID))
+                               :task "MIGRATE_PERSON_HANDLER"
+                               :interval "59 MINUTES"})
 
 (defn- take-with-error-handling
   "Takes message from queue and executes handler function with message.
@@ -187,3 +192,13 @@
            (Thread/sleep 10000))))
      (catch Exception e
        (log/error e "Syncing participant ONR data failed"))))
+
+(defmethod ig/init-key ::migrate-person-handler [_ {:keys [db]}]
+  {:pre [(some? db)]}
+  #(try
+     (when (job-db/try-to-acquire-lock! db person-migrator-conf)
+       (log/info "Person migration started")
+       (let [migrated-count (person/migrate-persons! db)]
+         (log/info (str migrated-count " registrations migrated to person table"))))
+     (catch Exception e
+       (log/error e "Person migration failed"))))
