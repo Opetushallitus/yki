d6569d15cb045ca26d9553c27292b1645745d2f5
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-07-01 12:25:33 +0300

    Log out weakly authenticated users after redirecting to Paytrail

--- src/yki/handler/exam_payment_new.clj
+++ src/yki/handler/exam_payment_new.clj
@@ -178,7 +178,10 @@
       (GET "/:id/redirect" {session :session}
         :path-params [id :- ::ys/registration_id]
         :query-params [lang :- ::ys/language-code]
-        (redirect-to-paytrail db payment-helper url-helper lang session id)))
+        (let [weakly-authenticated? (= "EMAIL" (:auth-method session))]
+          (cond-> (redirect-to-paytrail db payment-helper url-helper lang session id)
+                  weakly-authenticated?
+                  (assoc :session nil)))))
     (context routing/paytrail-payment-v2-root []
       ; TODO Is it safe to delete this whole endpoint?
       ; After removing support for redirecting to old UI, this is now identical to ...-v3-root
