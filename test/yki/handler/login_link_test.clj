898f9fb32de6bc1db0407367cb3e69a85ade1f0d
Laura Ketola <laura.ketola@gofore.com>
2026-04-30 13:36:33 +0300

    Change registration form url to /exam-session-id/registration-id

--- test/yki/handler/login_link_test.clj
+++ test/yki/handler/login_link_test.clj
@@ -52,7 +52,7 @@
               _                (base/body-as-json response)
               email-request    (pgq/take email-q)]
           (is (= (count code) 64))
-          (is (= success-redirect (str "http://yki.localhost:" port "/yki/ilmoittautuminen/tutkintotilaisuus/1?registrationId=")))
+          (is (= success-redirect (str "http://yki.localhost:" port "/yki/ilmoittautuminen/tutkintotilaisuus/1/")))
           (is (= (:status response) 200))
           (testing "email send request should be send to job queue"
             (is (= (:subject email-request) "Ilmoittautuminen (YKI): Suomi perustaso - Omenia, 27.1.2018"))
