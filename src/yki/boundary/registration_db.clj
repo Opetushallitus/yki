8b2bb3da4bb06060b1a0918d887cb0bde278a915
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-06-09 09:14:57 +0300

    Added API to get registration details

--- src/yki/boundary/registration_db.clj
+++ src/yki/boundary/registration_db.clj
@@ -50,7 +50,8 @@
   (lift-registration-from-queue! [db exam-session-id send-email!])
   (expire-queued-registrations-after-exam-date! [db])
   (get-free-registration [db registration-id])
-  (check-registration-id-matches-session [db registration-id participant-id external-user-id]))
+  (check-registration-id-matches-session [db registration-id participant-id external-user-id])
+  (get-registration-details-by-id [db registration-id]))
 
 (defn get-registration-data-with-tx
   [tx registration-id participant-id lang]
@@ -271,4 +272,7 @@
   (check-registration-id-matches-session
     [{:keys [spec]} registration-id participant-id external-user-id]
     (first (q/check-registration-id-matches-session spec {:id registration-id :participant_id participant-id :external_user_id external-user-id})))
+  (get-registration-details-by-id
+    [{:keys [spec]} registration-id]
+    (first (q/select-registration-details-by-id spec {:id registration-id})))
   )
