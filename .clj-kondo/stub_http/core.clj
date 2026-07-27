fbb3b3d2fb280e292ca833907c3344fcdfabaaff
Laura Ketola <laura.ketola@gofore.com>
2026-07-27 21:45:38 +0300

    Fix lint

--- .clj-kondo/stub_http/core.clj
+++ .clj-kondo/stub_http/core.clj
@@ -2,12 +2,14 @@
 
 ; Dummy implementation of with-routes! that lets clj-kondo know
 ; that `server` and `port` are defined by the with-routes! macro.
+; `server` is modelled with a derefable `:routes` and a numeric `:port` so that
+; `@(:routes server)` type-checks; newer clj-kondo errors on dereferencing nil.
 (defmacro with-routes! [bindings-or-routes & more-args]
   (let [[bindings routes body] (if (vector? bindings-or-routes)
                             [bindings-or-routes (first more-args) (rest more-args)]
                             [[] bindings-or-routes more-args])]
     `(let ~bindings
-       (let [~'server nil
+       (let [~'server {:routes (atom []) :port 0}
              ~'port (:port ~'server)]
          ~routes
          ~@body))))
